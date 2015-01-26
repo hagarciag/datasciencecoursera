8bdd78c2d6157f60f0762739d3de7175bac4c7a7
Hernan Alonso Garcia Gallego <hagarciag@gmail.com>
2015-01-25 20:56:13 -0500

    improved version of the best hodpital

--- ProgrammingAssignment3/best.r
+++ ProgrammingAssignment3/best.r
@@ -1,4 +1,4 @@
-best <- function(state, outcome) {
+best <- function(state, outcome=NULL) {
   ## Read outcome data
   ## Check that state and outcome are valid
   ## Return hospital name in that state with lowest 30-day death
@@ -6,10 +6,22 @@ best <- function(state, outcome) {
   
   #State
   #Hospital.Name
-#   Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack
-#   Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure
-#   Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia
+  #invalid state
+  if(outcome=="heart attack")
+    outcome<-"Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+  else if(outcome=="heart failure")
+    outcome<-"Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+  else if(outcome=="pneumonia")
+    outcome<-"Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+  else{
+    print("invalid outcome")
+    stop 
+  }
+  
   file<-read.csv(file = "ProgrammingAssignment3/outcome-of-care-measures.csv")
-  statehospitalnameoutcome <- file[file["State"]==state, c("Hospital.Name", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack")]
-  statehospitalnameoutcome
+  statehospitalnameoutcome <- file[file["State"]==state & file[outcome]!="Not Available", c("Hospital.Name", outcome)]
+  #besthospital<-min(as.numeric(as.matrix(statehospitalnameoutcome[outcome])), na.rm = TRUE)
+  minimail<-print(min(as.numeric(as.matrix(statehospitalnameoutcome[outcome]))))
+  besthospital<-statehospitalnameoutcome[as.numeric(as.matrix(statehospitalnameoutcome[outcome])) == minimail, c("Hospital.Name", outcome)]  
+  besthospital
 }
